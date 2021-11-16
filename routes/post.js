8e7c3aaffea090beab3d0442400e2b9714b1a2a9
phamhai-laptop <phamcong.hai.90@gmail.com>
2021-11-16 17:35:58 +0700

    fixed comment API

--- routes/post.js
+++ routes/post.js
@@ -4,9 +4,26 @@ const mysql = require("../config/mysql");
 const htmlEntities = require("html-entities");
 const Auth = require("../middleware/Auth");
 
+const sqlQuery = function (sql) {
+  return new Promise(function (resolve, reject) {
+    mysql.db.query(sql, function (error, result, fields) {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(result);
+      }
+    });
+  });
+};
+
 // API:GET /post/:id
-router.get("/:id", Auth, async (req, res) => {
+router.get("/:id", Auth, (req, res) => {
   //   console.log(req.query);
+  var response = {
+    success: true,
+    postData: {},
+    allComments: [],
+  };
   try {
     let postId = req.params.id;
     if (postId < 0 || isNaN(postId)) {
@@ -15,28 +32,30 @@ router.get("/:id", Auth, async (req, res) => {
     let queryFieldsPost =
       "posts.post_id AS postId,posts.user_id AS userId, avatar, content, likes, comments, time, name, users.blue_tick AS blueTick, images.link AS image";
     let sqlPost = `SELECT ${queryFieldsPost} FROM posts JOIN users ON posts.user_id = users.user_id LEFT JOIN images ON posts.post_id = images.post_id WHERE posts.post_id = ${postId} `; //AND posts.deleted = 0
-    mysql.db.query(sqlPost, function (error, postData, fields) {
-      if (error) throw error;
-      if (postData[0]) {
-        // query comments
-        if (postData[0].comments) {
-          let sqlCmt = `SELECT cmt_id AS cmtId, cmt_content AS cmtContent, user_id AS userCmtId, time AS cmtTime FROM comment_table WHERE post_id = ${postId} AND deleted = 0`; // AND deleted = 0
-          mysql.db.query(sqlCmt, (error, cmtArr, fields) => {
-            if (error) throw error;
-            res.json({
-              success: true,
-              postData: postData[0],
-              allComments: cmtArr,
-            });
-          });
+
+    sqlQuery(sqlPost)
+      .then((postData) => {
+        if (postData[0]) {
+          // query comments
+          response = { ...response, postData: { ...postData[0] } };
+          if (postData[0].comments) {
+            let sqlCmt = `SELECT cmt_id AS cmtId, post_id AS postId, cmt_content AS cmtContent, comment_table.user_id AS userCmtId, users.name, users.avatar,users.blue_tick AS blueTick, time AS cmtTime FROM comment_table LEFT JOIN users ON comment_table.user_id = users.user_id WHERE post_id = ${postId} AND deleted = 0 ORDER BY time DESC`; // AND deleted = 0
+            return sqlQuery(sqlCmt);
+          }
+        } else {
+          throw Error("404 not found");
         }
-      } else {
+      })
+      .then((allComments) => {
+        response = { ...response, allComments };
+        res.json(response);
+      })
+      .catch((err) => {
         res.json({
           success: false,
-          message: "404 not found",
+          message: err.message,
         });
-      }
-    });
+      });
   } catch (error) {
     res.json({
       success: false,
@@ -47,22 +66,49 @@ router.get("/:id", Auth, async (req, res) => {
 
 // API:POST /post/:id/cmt
 router.post("/:id/cmt", Auth, async (req, res) => {
+  var response = {
+    success: true,
+    message: "comment successful",
+    cmtId: null,
+  };
   try {
     let postId = req.params.id;
-    let comment = req.body.comment;
+    let comment = htmlEntities.encode(req.body.comment, {
+      mode: "nonAsciiPrintable", // get raw text/html
+    });
     let userId = req.userId; // userId comments at postId with data
 
-    let sqlInsertCmt = `INSERT INTO comment_table (cmt_content, post_id, user_id) VALUES (${mysql.db.escape(
-      comment
-    )}, '${postId}', '${userId}')`;
+    let sqlFindPost = `SELECT post_id AS postId, comments FROM posts WHERE post_id = ${postId}`;
+    let sqlInsertCmt = `INSERT INTO comment_table (cmt_content, post_id, user_id) VALUES ('${comment}', '${postId}', '${userId}')`;
 
-    mysql.db.query(sqlInsertCmt, function (error, result) {
-      if (error) throw error;
-      res.json({
-        success: true,
-        message: "Comment successfull",
+    sqlQuery(sqlFindPost)
+      .then((post) => {
+        if (post[0]) {
+          return sqlQuery(sqlInsertCmt);
+        } else {
+          throw Error("404 not found");
+        }
+      })
+      .then((insertResult) => {
+        response.cmtId = insertResult.insertId;
+        return sqlQuery(
+          `SELECT cmt_id FROM comment_table WHERE post_id = ${postId}`
+        );
+      })
+      .then((cmtCounter) => {
+        return sqlQuery(
+          `UPDATE posts SET comments = ${cmtCounter.length} WHERE post_id = ${postId}`
+        );
+      })
+      .then((result) => {
+        res.json(response);
+      })
+      .catch((err) => {
+        res.json({
+          success: false,
+          message: err.message,
+        });
       });
-    });
   } catch (error) {
     res.json({
       success: false,
