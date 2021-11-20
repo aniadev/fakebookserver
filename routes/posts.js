39f1fb9478aaa7df37a98b8eb4bd3c396a05c19c
phamhai-laptop <phamcong.hai.90@gmail.com>
2021-11-20 15:13:27 +0700

    fix callback APIs

--- routes/posts.js
+++ routes/posts.js
@@ -4,25 +4,46 @@ const mysql = require("../config/mysql");
 const htmlEntities = require("html-entities");
 const Auth = require("../middleware/Auth");
 
+const sqlQuery = function (sql) {
+  return new Promise(function (resolve, reject) {
+    mysql.db.query(sql, function (error, result, fields) {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(result);
+      }
+    });
+  });
+};
+
 // API:GET /posts
-router.get("/", Auth, async (req, res) => {
+router.get("/", Auth, (req, res) => {
   //   console.log(req.query);
   try {
     const userId = req.userId;
+    if (isNaN(req.query._page)) {
+      throw Error("_page invalid");
+    }
     const page = req.query._page > 0 ? req.query._page - 1 : 0;
     const limit = req.query._limit || 10;
     const offset = req.query._offset || page * limit || 0;
     const queryFields =
       "posts.post_id AS postId,posts.user_id AS userId, avatar, content, likes, comments, posts.time, name, users.blue_tick AS blueTick, images.link AS image, like_table.like_id AS likeId";
     const sql = `SELECT ${queryFields} FROM posts JOIN users ON posts.user_id = users.user_id LEFT JOIN images ON posts.post_id = images.post_id LEFT JOIN like_table ON like_table.user_id = ${userId} AND like_table.post_id = posts.post_id WHERE posts.deleted = 0 ORDER BY time DESC LIMIT ${limit} OFFSET ${offset}`;
-    mysql.db.query(sql, function (error, postList) {
-      if (error) throw error;
-      res.json({
-        success: true,
-        page: page + 1,
-        posts: postList,
+    sqlQuery(sql)
+      .then((postList) => {
+        res.json({
+          success: true,
+          page: page + 1,
+          posts: postList,
+        });
+      })
+      .catch((err) => {
+        res.json({
+          success: false,
+          message: err.message,
+        });
       });
-    });
   } catch (error) {
     res.json({
       success: false,
@@ -32,64 +53,52 @@ router.get("/", Auth, async (req, res) => {
 });
 
 // API:POST /posts/create
-router.post("/create", Auth, async (req, res) => {
-  let data = req.body;
-  let userId = req.userId;
-  let content = htmlEntities.encode(data.content, {
-    mode: "nonAsciiPrintable", // get raw text/html
-  });
-  let images = data.imageLinks;
-  // console.log(req.body);
-  // console.log(content);
-  let sqlPost = `INSERT INTO posts (user_id, content) VALUES ('${userId}', '${content}')`;
-  // const sqlImg = `INSERT INTO images (link, post_id) VALUES ('${data.images}', '${result.insert_id}')`;
-
-  // use callback function
-  if (!images) {
-    try {
-      mysql.db.query(sqlPost, function (error, result) {
-        if (error) throw error;
-        if (result) {
-          res.json({
-            success: true,
-            message: "create post successful",
-            postCreated: {
-              postId: result.insertId,
-              userId: req.userId,
-              content,
-              imageLinks: images,
-            },
-          });
-        }
-      });
-    } catch (error) {
-      console.log("🚀 ~ file: posts.js ~ line 41 ~ router.post ~ error", error);
+router.post("/create", Auth, (req, res) => {
+  try {
+    let data = req.body;
+    let userId = req.userId;
+    let content = htmlEntities.encode(data.content, {
+      mode: "nonAsciiPrintable", // get raw text/html
+    });
+    if (!content) {
+      throw Error("Content invalid");
     }
-  } else {
-    try {
-      mysql.db.query(sqlPost, function (error, result) {
-        if (error) throw error;
-        // console.log(result);
-        let sqlImg = `INSERT INTO images (link, post_id) VALUES ('${images}', '${result.insertId}')`;
-        mysql.db.query(sqlImg, function (error, result) {
-          if (error) throw error;
-          if (result) {
-            res.json({
-              success: true,
-              message: "create post successful",
-              postCreated: {
-                postId: result.insertId,
-                userId: req.userId,
-                content,
-                imageLinks: images,
-              },
-            });
-          }
+    let images = data.imageLinks;
+    let sqlPost = `INSERT INTO posts (user_id, content) VALUES ('${userId}', '${content}')`;
+    let newPostId;
+    sqlQuery(sqlPost)
+      .then((result) => {
+        newPostId = result.insertId;
+        if (!images) {
+          return true;
+        } else {
+          let sqlImg = `INSERT INTO images (link, post_id) VALUES ('${images}', '${result.insertId}')`;
+          return sqlQuery(sqlImg);
+        }
+      })
+      .then((result) => {
+        res.json({
+          success: true,
+          message: "create post successful",
+          postCreated: {
+            postId: newPostId,
+            userId: req.userId,
+            content,
+            imageLinks: images,
+          },
+        });
+      })
+      .catch((err) => {
+        res.json({
+          success: false,
+          message: err.message,
         });
       });
-    } catch (error) {
-      console.log("🚀 ~ file: posts.js ~ line 90 ~ router.post ~ error", error);
-    }
+  } catch (error) {
+    res.json({
+      success: false,
+      message: error.message,
+    });
   }
 });
 
@@ -99,35 +108,44 @@ router.post("/create", Auth, async (req, res) => {
 // }
 
 // API:POST /posts/delete
-router.post("/delete", Auth, async (req, res) => {
+router.post("/delete", Auth, (req, res) => {
   try {
     let postId = req.body.postId;
+    if (isNaN(postId)) {
+      throw Error("PostId invalid");
+    }
     let userId = req.userId;
     let checkPost = `SELECT deleted FROM posts WHERE post_id = '${postId}' AND user_id = '${userId}'`;
-    mysql.db.query(checkPost, function (error, result) {
-      if (error) throw error;
-      if (result[0].deleted === 0) {
-        // chua bi xoa ?
-        let sqlDelete = `UPDATE posts SET deleted = 1 WHERE post_id = '${postId}' AND user_id = '${userId}'`;
-        mysql.db.query(sqlDelete, function (error, result) {
-          if (error) throw error;
-          res.json({
-            success: true,
-            message: "delete post successful",
-          });
+    sqlQuery(checkPost)
+      .then((result) => {
+        if (result[0]) {
+          if (result[0].deleted) {
+            return true;
+          } else {
+            // chua bi xoa ?
+            let sqlDelete = `UPDATE posts SET deleted = 1 WHERE post_id = '${postId}' AND user_id = '${userId}'`;
+            return sqlQuery(sqlDelete);
+          }
+        } else {
+          throw Error("Permission invalid");
+        }
+      })
+      .then(() => {
+        res.json({
+          success: true,
+          message: "delete post successful",
         });
-      } else {
+      })
+      .catch((error) => {
         res.json({
           success: false,
-          message: "delete post failed",
+          message: error.message,
         });
-      }
-    });
+      });
   } catch (error) {
-    console.log(error);
     res.json({
       success: false,
-      message: "delete post failed",
+      message: error.message,
     });
   }
 });
--- routes/user.js
+++ routes/user.js
@@ -3,125 +3,142 @@ const router = express.Router();
 const mysql = require("../config/mysql");
 const Auth = require("../middleware/Auth");
 
-// https://fakebook.com:8080/user/
+const sqlQuery = function (sql) {
+  return new Promise(function (resolve, reject) {
+    mysql.db.query(sql, function (error, result, fields) {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(result);
+      }
+    });
+  });
+};
+
+// https://fakebook.com:8080/user?_id=2
 router.get("/", Auth, async (req, res) => {
-  let userId = parseInt(req.query._id, 10);
-  // console.log(parseInt(userId, 10));
-  if (userId) {
-    try {
-      // const queryFields = "user_id, name, username, avatar, email";
-      const sql = `SELECT user_id, name, username, avatar, email, birthday, address, cover_photo, blue_tick FROM fastbook.users WHERE user_id = ${userId}`;
-      // console.log(sql);
-      mysql.db.query(sql, function (err, result) {
-        if (err) throw err;
-        // console.log(result);
-        if (!result[0]) {
-          return res.json({
-            success: false,
-            message: "No data",
+  try {
+    let userId = req.query._id;
+    if (isNaN(userId) || !userId) {
+      throw Error("User ID invalid");
+    }
+    const sql = `SELECT user_id, name, username, avatar, email, birthday, address, cover_photo, blue_tick FROM fastbook.users WHERE user_id = ${userId}`;
+    sqlQuery(sql)
+      .then((result) => {
+        if (result[0]) {
+          let userData = {
+            userId: result[0].user_id,
+            name: result[0].name,
+            username: result[0].username,
+            email: result[0].email,
+            avatar: result[0].avatar,
+            birthday: result[0].birthday,
+            address: result[0].address,
+            coverPhoto: result[0].cover_photo,
+            blueTick: result[0].blue_tick,
+          };
+          res.json({
+            success: true,
+            userData,
           });
+        } else {
+          throw Error("404 not found");
         }
-        let userData = {
-          userId: result[0].user_id,
-          name: result[0].name,
-          username: result[0].username,
-          email: result[0].email,
-          avatar: result[0].avatar,
-          birthday: result[0].birthday,
-          address: result[0].address,
-          coverPhoto: result[0].cover_photo,
-          blueTick: result[0].blue_tick,
-        };
+      })
+      .catch((err) => {
         res.json({
-          success: true,
-          userData,
+          success: false,
+          message: err.message,
         });
       });
-    } catch (error) {
-      res.json({
-        success: false,
-        message: error.message,
-      });
-    }
-  } else {
+  } catch (error) {
     res.json({
       success: false,
-      message: "userAPI/profile: not enough query fields",
+      message: error.message,
     });
   }
 });
 
 // https://fakebook.com:8080/user/posts
 router.get("/posts", Auth, async (req, res) => {
-  let userId = parseInt(req.query._id, 10);
-  // console.log(parseInt(userId, 10));
-  if (userId) {
-    try {
-      const queryFields =
-        "posts.post_id AS postId, posts.user_id AS userId, content, likes, comments, time, images.link AS image";
-      const sql = `SELECT ${queryFields} FROM posts LEFT JOIN images ON posts.post_id = images.post_id WHERE posts.user_id = ${userId} AND posts.deleted = 0 ORDER BY time DESC`;
-      // console.log(sql);
-      mysql.db.query(sql, function (err, result) {
-        if (err) throw err;
-        if (result) {
-          res.json({
-            success: true,
-            posts: result,
-          });
-        }
-      });
-    } catch (error) {
-      res.json({
-        success: false,
-        message: error.message,
-      });
+  try {
+    let userId = req.query._id;
+    if (isNaN(userId) || !userId) {
+      throw Error("User ID invalid");
     }
-  } else {
+    const queryFields =
+      "posts.post_id AS postId, posts.user_id AS userId, content, likes, comments, posts.time, images.link AS image, like_table.like_id AS likeId";
+    const sql = `SELECT ${queryFields} FROM posts LEFT JOIN images ON posts.post_id = images.post_id LEFT JOIN like_table ON like_table.user_id = ${userId} AND like_table.post_id = posts.post_id WHERE posts.user_id = ${userId} AND posts.deleted = 0 ORDER BY time DESC`;
+    sqlQuery(sql)
+      .then((results) => {
+        res.json({
+          success: true,
+          posts: results,
+        });
+      })
+      .catch((error) => {
+        res.json({
+          success: false,
+          message: error.message,
+        });
+      });
+  } catch (error) {
     res.json({
       success: false,
-      message: "userAPI/posts: not enough query fields",
+      message: error.message,
     });
   }
 });
 
-// https://fakebook.com:8080/user/posts
-router.post("/avatar", Auth, async (req, res) => {
+// https://fakebook.com:8080/user/avatar
+router.post("/avatar", Auth, (req, res) => {
   try {
     let data = req.body;
     let userId = req.userId;
     let avatar = data.avatarLink;
+    if (!avatar) {
+      throw Error("Avatar Link invalid");
+    }
     let content = "Đã cập nhật ảnh đại diện.";
     // update avatar
-    let sqlAvatar = `UPDATE users SET avatar = '${avatar}' WHERE user_id = ${userId}`; // Callback helllllllllllllll
-    mysql.db.query(sqlAvatar, function (error, result) {
-      if (error) throw error;
-      if (result) {
+    let sqlAvatar = `UPDATE users SET avatar = ${mysql.db.escape(
+      avatar
+    )} WHERE user_id = ${userId}`;
+    let postId;
+    sqlQuery(sqlAvatar) // update avatar
+      .then(() => {
         // create post
         let sqlPost = `INSERT INTO posts (user_id, content) VALUES ('${userId}', '${content}')`;
-        mysql.db.query(sqlPost, function (error, result) {
-          if (error) throw error;
-          // console.log(result);
-          let sqlImg = `INSERT INTO images (link, post_id) VALUES ('${avatar}', '${result.insertId}')`;
-          mysql.db.query(sqlImg, function (error, result) {
-            if (error) throw error;
-            if (result) {
-              res.json({
-                success: true,
-                message: "update avatar successful",
-                postCreated: {
-                  postId: result.insertId,
-                  userId: req.userId,
-                  content,
-                  imageLinks: avatar,
-                },
-              });
-            }
-          });
+        return sqlQuery(sqlPost);
+      })
+      .then((sqlPostResult) => {
+        postId = sqlPostResult.insertId;
+        let sqlImg = `INSERT INTO images (link, post_id) VALUES ('${avatar}', '${postId}')`;
+        return sqlQuery(sqlImg);
+      })
+      .then(() => {
+        res.json({
+          success: true,
+          message: "update avatar successful",
+          postCreated: {
+            postId: postId,
+            userId: req.userId,
+            content,
+            imageLinks: mysql.db.escape(avatar),
+          },
         });
-      }
-    });
+      })
+      .catch((err) => {
+        res.json({
+          success: false,
+          message: err.message,
+        });
+      });
   } catch (error) {
-    console.log("🚀 ~ file: user.js ~ line 124 ~ router.post ~ error", error);
+    res.json({
+      success: false,
+      message: error.message,
+    });
   }
 });
 
